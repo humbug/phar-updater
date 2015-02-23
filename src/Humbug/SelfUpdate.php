d619fdcde979152f66fa01a73c050fe6a66f8f6f
Padraic Brady <padraic.brady@gmail.com>
2015-02-23 21:52:08 +0000

    Merely a basic skeleton awaiting attention

---
+++ src/Humbug/SelfUpdate.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Humbug
+ *
+ * @category   Humbug
+ * @package    Humbug
+ * @copyright  Copyright (c) 2015 Pádraic Brady (http://blog.astrumfutura.com)
+ * @license    https://github.com/padraic/humbug/blob/master/LICENSE New BSD License
+ */
+
+namespace Humbug;
+
+class SelfUpdate
+{
+
+    public function __construct()
+    {
+        
+    }
+}
